608a8ef7ee042abb527bc9212b6e77383bb51674
ACL-404 <zxcsd0919313688@gmail.com>
2025-09-26 10:44:32 +0800

    250926
    02

--- Assets/PolygonMilitary/scripts/moveCarmer.cs
+++ Assets/PolygonMilitary/scripts/moveCarmer.cs
@@ -3,10 +3,14 @@ using UnityEngine;
 public class moveCarmer : MonoBehaviour
 {
     [SerializeField] private float speed = 3.5f;
+    [SerializeField] private float rotateSpeed = 100f; // 滑鼠旋轉速度
+    private float rotationX = 0f;
+    private float rotationY = 0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
@@ -14,19 +18,27 @@ public class moveCarmer : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow ))
         {
-            transform.Translate(Vector3.forward * 3.5f*Time.deltaTime);
+            transform.Translate(Vector3.forward * 30f*Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) 
         {
-            transform.Translate(Vector3.back * 3.5f * Time.deltaTime);
+            transform.Translate(Vector3.back * 30f * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Translate(Vector3.right * 3.5f * Time.deltaTime);
+            transform.Translate(Vector3.right * 30f * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Translate(Vector3.left * 3.5f * Time.deltaTime);
+            transform.Translate(Vector3.left * 30f * Time.deltaTime);
         }
+        float mouseX = Input.GetAxis("Mouse X") * rotateSpeed * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * rotateSpeed * Time.deltaTime;
+
+        rotationX -= mouseY;
+        rotationY += mouseX;
+        rotationX = Mathf.Clamp(rotationX, -80f, 80f); // 限制上下旋轉角度
+
+        transform.rotation = Quaternion.Euler(rotationX, rotationY, 0f);
     }
 }
